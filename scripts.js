e61d57101fc978916d1a8d63ce8ee39d0204c015
mufaddal chharchhodawala <mufazzalkc@gmail.com>
2024-06-26 12:22:35 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const toggleBtn = document.querySelector('.navbar__mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
+
+    toggleBtn.addEventListener('click', function(){
+        mobileMenu.classList.toggle('active');
+    });
+});
+
+window.addEventListener('scroll', function(){
+    const navbar = document.querySelector('.navbar');
+
+    if(window.scrollY > 0){
+        navbar.classList.add('navbar--scroll');
+    }else{
+        navbar.classList.remove('navbar--scroll');
+    }
+})
